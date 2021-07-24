0972fafe2952349e72b8b3fc43b0fa263e30dfcd
freebuds <ga77493@gmail.com>
2021-07-24 15:06:22 +0100

    - Added Intro Activity

---
+++ app/src/main/java/com/bskyb/skyrewards/view/SRWIntroActivity.kt
@@ -0,0 +1,11 @@
+package com.bskyb.skyrewards.view.main
+
+import android.os.Bundle
+import com.bskyb.skyrewards.R
+
+class SRWIntroActivity : SRWBaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+    }
+}
