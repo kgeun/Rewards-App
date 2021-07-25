ab9694f2e8b8114bf7cd49ff352a6ab8ae8d5539
freebuds <ga77493@gmail.com>
2021-07-25 18:46:16 +0100

    - Organized File structures

--- app/src/main/java/com/bskyb/skyrewards/view/SRWMainActivity.kt
+++ app/src/main/java/com/bskyb/skyrewards/view/SRWMainActivity.kt
@@ -1,30 +1,42 @@
 package com.bskyb.skyrewards.view
 
-import android.content.Context
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsControllerCompat
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
 import com.bskyb.skyrewards.R
 import com.bskyb.skyrewards.analytics.SRWAnalytics
 import com.bskyb.skyrewards.databinding.ActivityMainBinding
+import com.bskyb.skyrewards.service.SRWServiceHelper
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 open class SRWMainActivity: AppCompatActivity() {
-
-    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private var statusbarHeight = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        setTransparentStatusBar()
+        sendAnalytics()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initStatusBarPadding()
+        SRWServiceHelper.startServices(this)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        SRWServiceHelper.stopServices(this)
+    }
+
+    private fun setTransparentStatusBar() {
         // Basic status bar Height
         statusbarHeight = resources.getDimensionPixelSize(R.dimen.statusbar_height)
         val decorView: View = window.decorView
@@ -37,13 +49,6 @@ open class SRWMainActivity: AppCompatActivity() {
             wic.isAppearanceLightStatusBars = true
             WindowCompat.setDecorFitsSystemWindows(window, false)
         }
-
-        sendAnalytics()
-    }
-
-    override fun onStart() {
-        super.onStart()
-        initStatusBarPadding()
     }
 
     private fun initStatusBarPadding() {
