93449af948833832826a0140921a81ad11b4a646
freebuds <ga77493@gmail.com>
2021-07-25 18:47:34 +0100

    - Added Hilt code

--- app/src/main/java/com/bskyb/skyrewards/SRWApplication.kt
+++ app/src/main/java/com/bskyb/skyrewards/SRWApplication.kt
@@ -4,7 +4,9 @@ import android.app.Application
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bskyb.skyrewards.R
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class SRWApplication : Application() {
     companion object {
         lateinit var instance: SRWApplication
