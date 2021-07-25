903e7c45b96dbe4d86d673f708279b1e7fffd16a
freebuds <ga77493@gmail.com>
2021-07-25 22:40:56 +0100

    - Added Third screen's logic

--- app/src/main/java/com/bskyb/skyrewards/view/fragment/SRWAccountNumberFragment.kt
+++ app/src/main/java/com/bskyb/skyrewards/view/fragment/SRWAccountNumberFragment.kt
@@ -1,17 +1,25 @@
 package com.bskyb.skyrewards.view.fragment
 
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.widget.Toast
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.bskyb.skyrewards.analytics.SRWAnalytics
 import com.bskyb.skyrewards.databinding.FragmentAccountNumberBinding
 import com.bskyb.skyrewards.view.SRWBaseFragment
+import com.bskyb.skyrewards.view.SRWMainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SRWAccountNumberFragment : SRWBaseFragment() {
 
     private lateinit var binding: FragmentAccountNumberBinding
+    private val mainViewModel: SRWMainViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,21 +30,47 @@ class SRWAccountNumberFragment : SRWBaseFragment() {
         binding = FragmentAccountNumberBinding.inflate(inflater, container, false)
 
         setBtnActions()
+        observeResult()
 
         SRWAnalytics.sendView("SRWIntroFragment")
         return binding.root
     }
 
-    fun setBtnActions() {
+    private fun observeResult() {
+        mainViewModel.myAccountNumber.observe(this) {
+            Toast.makeText(activity, "accountNuber : ${it}", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun setBtnActions() {
         binding.backBtnText.setOnClickListener {
             findNavController().popBackStack()
             SRWAnalytics.sendClick("BackBtn_${javaClass.simpleName}")
         }
 
         binding.nextBtnText.setOnClickListener {
-            // ToDo
+            enterNumberFinished()
             SRWAnalytics.sendClick("NextBtn_${javaClass.simpleName}")
+        }
+
+        binding.accountNumberEditText.setOnEditorActionListener { view, actionId, event ->
+            if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
+                SRWAnalytics.sendClick("KeyboardDoneBtn_${javaClass.simpleName}")
+                enterNumberFinished()
+            }
+            false
         }
     }
 
+    private fun enterNumberFinished() {
+        validateAccountNumber(binding.accountNumberEditText.text.toString())
+    }
+
+    private fun validateAccountNumber(accountNumberString: String) {
+        if (accountNumberString.length != 12) {
+            Toast.makeText(activity, "Please enter all 12-digit account numbers.", Toast.LENGTH_SHORT).show()
+        } else {
+            mainViewModel.myAccountNumber.postValue(accountNumberString)
+        }
+    }
 }
