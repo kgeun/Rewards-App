3c20edf96b68e598a6860af0a2d8313be69a4bf6
freebuds <ga77493@gmail.com>
2021-07-26 20:35:42 +0100

    - ServiceResult outputID to resultCode

--- app/src/main/java/com/bskyb/skyrewards/data/enums/SRWServiceResult.kt
+++ app/src/main/java/com/bskyb/skyrewards/data/enums/SRWServiceResult.kt
@@ -1,6 +1,6 @@
 package com.bskyb.skyrewards.data.enums
 
-enum class SRWServiceResult(val message: String, val outputId: Int) {
+enum class SRWServiceResult(val message: String, val resultCode: Int) {
     CUSTOMER_ELIGIBLE("Customer is eligible", 1),
     CUSTOMER_INELIGIBLE("Customer is not eligible", 0),
     TECHNICAL_FAILURE_ERROR1("Service technical failure (ResultsService Failure)", -99999),
